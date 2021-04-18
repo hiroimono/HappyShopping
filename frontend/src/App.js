a4ce810fe5f484dbff3832475307b8eb4c864aa7
Huseyin Eltutan <heltutan54@gmail.com>
2021-04-18 22:47:10 +0200

    Setup React

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <>
+            <h1>
+                Welcome to Happy Shopping
+            </h1>
+        </>
+    );
+}
+
+export default App;
