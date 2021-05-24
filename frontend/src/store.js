3d108e9d84d0a9f095db7e88c019acf700b2f7fb
hiroimono <heltutan@gmail.com>
2021-05-25 01:43:39 +0200

    adjust store for heroku

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,7 +14,7 @@
 /** Combined Version of State */
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'remote-redux-devtools';
+// import { composeWithDevTools } from 'remote-redux-devtools';
 import { combinedReducer } from './reducers/combinedReducer.js';
 import { cartItemsFromLocalStorage, userLoggedInFromLocalStorage } from './localStorage.js';
 
@@ -28,8 +28,8 @@ const middleware = [thunk];
 let composeEnhancers;
 let store;
 if (process.env.NODE_ENV === 'development') {
-    composeEnhancers = composeWithDevTools({ realtime: true, port: 8000 });
-    store = createStore(combinedReducer, initialState, composeEnhancers(applyMiddleware(...middleware)));
+    // composeEnhancers = composeWithDevTools({ realtime: true, port: 8000 });
+    // store = createStore(combinedReducer, initialState, composeEnhancers(applyMiddleware(...middleware)));
 } else {
     store = createStore(combinedReducer, initialState, applyMiddleware(...middleware));
 }
